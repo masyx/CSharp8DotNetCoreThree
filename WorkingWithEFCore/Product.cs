02ed9ce342e3b3ed3291bfa904a898c6aa9ea2d3
Sergey <rfmatveev@gmail.com>
2021-01-31 22:14:59 -0500

    01/31/21 just commit

---
+++ WorkingWithEFCore/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Packt.Shared
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 4)]
+        public string ProductName { get; set; }
+
+        [Column("UnitPrice", TypeName = "money")]
+        public decimal? Cost { get; set; } // property name != field name
+
+        [Column("UnitsInStock")]
+        public short? Stock { get; set; }
+
+        public bool Discontinued { get; set; }
+
+        // these to define foreign key relationship
+        // to the Categories table
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
--- WorkingWithEFCore/WorkingWithEFCore.cs
+++ WorkingWithEFCore/WorkingWithEFCore.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
 
 namespace WorkingWithEFCore
 {
@@ -6,7 +10,23 @@ namespace WorkingWithEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QueryingCategories();
+        }
+
+        static void QueryingCategories()
+        {
+            using (var db = new Northwind())
+            {
+                Console.WriteLine("Categories and how many products they have.");
+
+                // a query to get categories and they related products
+                IQueryable<Category> cats = db.Categories.Include(cat => cat.Products);
+
+                foreach(var cat in cats)
+                {
+                    Console.WriteLine($" Category '{cat.CategoryName}' has {cat.Products.Count} products");
+                }
+            }
         }
     }
 }
