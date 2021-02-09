978ed79fe6c32003335c3255a39f3260bc7365a2
Sergey <rfmatveev@gmail.com>
2021-02-09 10:12:16 -0500

    studying 02/09/2021

---
+++ LinqWithEFCore/LinqWithEFCoreClass.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace LinqWithEFCore
+{
+    class LinqWithEFCoreClass
+    {
+        static void Main(string[] args)
+        {
+            FilterAndSort();
+        }
+
+        // products that cost less than 10
+
+        static void FilterAndSort()
+        {
+            using (var db = new Northwind())
+            {
+                var products = db.Products.Where(product => product.UnitPrice < 10M)
+                    .OrderByDescending(product => product.UnitPrice)
+                    .Select(product => new
+                    {
+                        product.ProductID,
+                        product.ProductName,
+                        product.UnitPrice
+                    });
+
+                WriteLine("Products that cost less than $10:");
+                foreach (var item in products)
+                {
+                    WriteLine("{0}: {1} costs {2:$#,##0.00}",
+                      item.ProductID, item.ProductName, item.UnitPrice);
+                }
+                WriteLine();
+            }
+
+        }
+    }
+}
