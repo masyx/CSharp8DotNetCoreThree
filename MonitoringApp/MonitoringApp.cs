e7681e860f3ce1c65eace8b4514c9967955e0596
Sergey <rfmatveev@gmail.com>
2021-02-18 09:27:25 -0500

    02/18/2021 studying

---
+++ MonitoringApp/MonitoringApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Packt.Shared;
+
+namespace PerformanceAndScalability
+{
+    class MonitoringApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Processing, please wait...");
+            Recorder.Start();
+
+            // simulate a process that requires some memory resources
+            int[] largeArrayOfInt = Enumerable.Range(1, 10_000).ToArray();
+
+            // and takes some time to complete
+            System.Threading.Thread.Sleep(new Random().Next(5, 10) * 1000);
+
+            Recorder.Stop();
+        }
+    }
+}
