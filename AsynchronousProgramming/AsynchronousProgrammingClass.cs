351aa5ce1cab8308c206d51c81018227ccbc26d4
Sergey <rfmatveev@gmail.com>
2021-02-18 22:02:35 -0500

    02/18/2021 studying

---
+++ AsynchronousProgramming/AsynchronousProgrammingClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+
+namespace AsynchronousProgramming
+{
+    class AsynchronousProgrammingClass
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            //WriteLine("Running methods synchronously on one thread.");
+            //MethodA();
+            //MethodB();
+            //MethodC();
+
+            WriteLine("Running methods asynchronously on multiple threads.");
+            Task taskA = new Task(MethodA);
+            taskA.Start();
+            Task taskB = Task.Factory.StartNew(MethodB);
+            Task taskC = Task.Run(new Action(MethodC));
+
+            WriteLine($"{stopwatch.ElapsedMilliseconds:#,##0}ms elapsed.");
+        }
+
+        static void MethodA()
+        {
+            WriteLine("Starting Method A...");
+            Thread.Sleep(3000); // simulate three seconds of work WriteLine("Finished Method A.");
+        }
+        static void MethodB()
+        {
+            WriteLine("Starting Method B...");
+            Thread.Sleep(2000); // simulate two seconds of work WriteLine("Finished Method B.");
+        }
+        static void MethodC()
+        {
+            WriteLine("Starting Method C...");
+            Thread.Sleep(1000); // simulate one second of work WriteLine("Finished Method C.");
+        }
+    }
+}
--- MonitoringApp/MonitoringApp.cs
+++ MonitoringApp/MonitoringApp.cs
@@ -8,6 +8,12 @@ namespace PerformanceAndScalability
     {
         static void Main(string[] args)
         {
+            //FirstTest();
+            StringVsStringBuilderPerformance();
+        }
+
+        static void FirstTest()
+        {
             Console.WriteLine("Processing, please wait...");
             Recorder.Start();
 
@@ -17,6 +23,34 @@ namespace PerformanceAndScalability
             // and takes some time to complete
             System.Threading.Thread.Sleep(new Random().Next(5, 10) * 1000);
 
+            Recorder.Stop();
+        }
+
+        static void StringVsStringBuilderPerformance()
+        {
+            int[] largeArrayOfInt = Enumerable.Range(0, 50_000)
+                .Select(i => new Random().Next(0, 999))
+                .ToArray();
+            
+            Console.WriteLine("Using string with +");
+            Recorder.Start();
+            string s = string.Empty;
+
+            for (int i = 0; i < largeArrayOfInt.Length; i++)
+            {
+                s += largeArrayOfInt[i].ToString() + ", ";
+            }
+            Recorder.Stop();
+
+
+            Console.WriteLine($"\nUsing StringBuilder");
+            Recorder.Start();
+            var sb = new System.Text.StringBuilder();
+
+            for (int i = 0; i < largeArrayOfInt.Length; i++)
+            {
+                sb.Append(largeArrayOfInt[i]).Append(", ");
+            }   
             Recorder.Stop();
         }
     }
