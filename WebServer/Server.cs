7ae93cc48a3f32736b70e1d395aa21902e180367
SergeyM <rfmatveev@gmail.com>
2021-05-26 11:02:17 -0400

    GetLocalHostIPs() added

--- WebServer/Server.cs
+++ WebServer/Server.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -11,5 +13,15 @@ namespace Sergeys.WebServer
     public class Server
     {
         private static HttpListener listener;
+
+        private static List<IPAddress> GetLocalHostIPs()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            List<IPAddress> ret = host.AddressList.
+                Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
+
+            return ret;
+        }
     }
 }
