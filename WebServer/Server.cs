d648c97650c661605bc25bdf48216be5806ef242
SergeyM <rfmatveev@gmail.com>
2021-05-26 10:52:08 -0400

    Created a WebServer library project

---
+++ WebServer/Server.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Sergeys.WebServer
+{
+    /// <summary>
+    /// A lean and mean web server
+    /// </summary>
+    public class Server
+    {
+        private static HttpListener listener;
+    }
+}
