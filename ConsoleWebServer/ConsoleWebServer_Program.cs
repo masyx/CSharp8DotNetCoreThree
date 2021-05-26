a53e3ff59a5807a77f1021b40338c5c149725457
SergeyM <rfmatveev@gmail.com>
2021-05-26 12:27:29 -0400

    ConsoleWebServer project added

---
+++ ConsoleWebServer/ConsoleWebServer_Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Sergeys.WebServer;
+
+namespace ConsoleWebServer
+{
+    class ConsoleWebServer_Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Start();
+            Console.ReadLine();
+        }
+    }
+}
